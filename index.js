b7dbc9874a144b74555883046763a724a789e80a
Suruchi Suman <152915515+suruchi-suman@users.noreply.github.com>
2026-07-09 02:03:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Family Travel Tracker",
-  password: "password",
-  port: 5432,
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 db.connect();
 
